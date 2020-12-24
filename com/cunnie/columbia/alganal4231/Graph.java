ad20d2a434324465ad000feb11b3a06412215c2b
Brendan Cunnie <saintbrendan@gmail.com>
2020-12-24 15:53:11 -0500

    Add dfsSolution tests and logic

--- com/cunnie/columbia/alganal4231/Graph.java
+++ com/cunnie/columbia/alganal4231/Graph.java
@@ -8,6 +8,36 @@ public class Graph {
     private int[] p;
     private Color[] color;
     private java.util.Stack<EdgeWeight> stack = new Stack<>();
+    private int[] minValues;
+
+    int[] getMinSolution() {
+        minValues = new int[nodes.size()];
+        color = new Color[nodes.size()];
+        Arrays.fill(minValues, -1);
+        Arrays.fill(color, Color.WHITE);
+        for (int u = 0; u < nodes.size(); u++) {
+            if (minValues[u] == -1) {
+                dfsSolution(u, 1);
+            }
+        }
+        return minValues;
+    }
+
+    private void dfsSolution(int u, int i) {
+        minValues[u] = i;
+        color[u] = Color.GREY;
+        for (Edge e : nodes.get(u).adjacencyList) {
+            int v = e.to;
+            int solution = i + e.weight;
+            if (minValues[v] < solution) {
+                if (color[v] == Color.GREY) {
+                    throw new IllegalStateException("Inconsistent inequalities");
+                }
+                dfsSolution(v, solution);
+            }
+        }
+        color[u] = Color.BLACK;
+    }
 
     public class Node {
         private int nodeNumber;
--- com/cunnie/columbia/alganal4231/GraphTest.java
+++ com/cunnie/columbia/alganal4231/GraphTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class GraphTest {
     Graph g;
@@ -26,6 +25,105 @@ class GraphTest {
 
         assertTrue(g.isConsistent());
     }
+
+    @Test
+    void oneNodeSolutionIs1() {
+        g.node(0);
+
+        assertTrue(g.getMinSolution()[0] == 1);
+    }
+
+    @Test
+    void weakInequalityLoopSolutionIs1s() {
+        Node n0 = g.node(0);
+        Node n1 = g.node(1);
+        n0.lte(n1);
+        n1.lte(n0);
+
+        assertTrue(g.getMinSolution()[0] == 1);
+        assertTrue(g.getMinSolution()[1] == 1);
+    }
+
+    @Test
+    void weakStrongInequalityLoopSolutionThrowsException() {
+        Node n0 = g.node(0);
+        Node n1 = g.node(1);
+        n0.lt(n1);
+        n1.lte(n0);
+
+        assertThrows(IllegalStateException.class, () -> g.getMinSolution());
+    }
+
+    @Test
+    void longStrongInequalityLoopThrowsException() {
+        Node n0 = g.node(0);
+        Node n1 = g.node(1);
+        Node n2 = g.node(2);
+        Node n3 = g.node(3);
+        Node n4 = g.node(4);
+        n0.lte(n1);
+        n1.lte(n2);
+        n2.lt(n3);
+        n3.lte(n4);
+        n4.lte(n0);
+
+        assertThrows(IllegalStateException.class, () -> g.getMinSolution());
+    }
+
+    @Test
+    void longWeakInequalityHasSameMinvalue() {
+        Node n0 = g.node(0);
+        Node n1 = g.node(1);
+        Node n2 = g.node(2);
+        Node n3 = g.node(3);
+        Node n4 = g.node(4);
+        n0.lte(n1);
+        n1.lte(n2);
+        n2.lte(n3);
+        n3.lte(n4);
+        n4.lte(n0);
+
+        assertTrue(g.getMinSolution()[0] == 1);
+        assertTrue(g.getMinSolution()[1] == 1);
+        assertTrue(g.getMinSolution()[2] == 1);
+        assertTrue(g.getMinSolution()[3] == 1);
+        assertTrue(g.getMinSolution()[4] == 1);
+    }
+
+    @Test
+    void strongInequalitesAcrossShortSubtreesHasSolution() {
+        Node root = g.node(0);
+        Node leftNode = g.node(1);
+        Node rightNode = g.node(2);
+        root.lt(leftNode);
+        root.lt(rightNode);
+        leftNode.lt(rightNode);
+
+        assertTrue(g.getMinSolution()[0] == 1);
+        assertTrue(g.getMinSolution()[1] == 2); // leftNode
+        assertTrue(g.getMinSolution()[2] == 3); // rightNode
+    }
+
+    @Test
+    void strongInequalitesAcrossSubtreesHasSolution() {
+        Node root = g.node(0);
+        Node leftSubtree = g.node(1);
+        Node leftLeaf = g.node(2);
+        Node rightSubtree = g.node(3);
+        Node rightLeaf = g.node(4);
+        root.lt(leftSubtree);
+        leftSubtree.lt(leftLeaf);
+        root.lt(rightSubtree);
+        rightSubtree.lt(rightLeaf);
+        rightLeaf.lt(leftLeaf);
+
+        assertTrue(g.getMinSolution()[0] == 1);
+        assertTrue(g.getMinSolution()[1] == 2); // leftSubtree
+        assertTrue(g.getMinSolution()[2] == 4); // leftLeaf
+        assertTrue(g.getMinSolution()[3] == 2); // rightSubtree
+        assertTrue(g.getMinSolution()[4] == 3); // rightLeaf ( < leftLeaf)
+    }
+
 
     @Test
     void weakInequalityLoopIsConsistent() {
